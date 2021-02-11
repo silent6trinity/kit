1fdc55982f1c5e08ebf1963e5a987360eab631a6
silent6trinity <78173918+silent6trinity@users.noreply.github.com>
2021-02-11 10:30:47 -0500

    Update kit.sh
    Fixed sublime text issue because im dumb

--- kit.sh
+++ kit.sh
@@ -22,7 +22,8 @@ echo "$NC"
 # SYSTEM LEVEL UPDATES & INSTALLS
 function systemupdate() {
 	cd /home/kali/Downloads
-	wget -qO https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	sudo apt-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt install python3-pip -y
 	sudo apt update -y
