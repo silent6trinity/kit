35d07f498ff27497f612cccd4ec9f9ba64f99a93
silent6trinity <78173918+silent6trinity@users.noreply.github.com>
2021-04-22 10:26:44 -0400

    Made user dynamic
    Adjusted so it should function now for all users, not just 'kali'

--- kit.sh
+++ kit.sh
@@ -1,5 +1,24 @@
 #!/bin/zsh
 
+#TODO:
+# begin splitting tool setup (like webservers) into their own function(s)
+# create function for the checking whether or not the git folder exists or not already
+# change the packages from lines for each of these, to a for loop
+# change the install choice(s) to script arguments instead
+
+# Implement:
+#PACKAGES=$(cat pkg.txt)
+#for i in $PACKAGES; do
+#	sudo apt install $i
+#done
+#USER=$(whoami)
+#$USERDIR="/home/$user/Downloads/"
+#
+#
+#
+
+export user=`whoami`
+
 
 #### COLORS ####
 C=$(printf '\033')
@@ -9,19 +28,16 @@ Y="${C}[1;33m"
 NC="${C}[0m"
 ################
 
-echo "$Y Automated kit buildout script"
-echo "$NC"
 
-set -eu -o pipefail # If we fail due to some error, debug all lines
-sudo -n true
-test $? -eq 0 || exit 1 "$RED ERROR: This script must be run as root"
-echo "$NC"
+#USER=$(whoami)
+USERDIR="/home/$user/Downloads/"
+
 
 ###################
 #### FUNCTIONS ####
 # SYSTEM LEVEL UPDATES & INSTALLS
 function systemupdate() {
-	cd /home/kali/Downloads
+	cd $USERDIR
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo apt-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -36,10 +52,10 @@ function systemupdate() {
 
 # SOFTWARE TOOL PACKAGE INSTALLS
 function softwareupdate() {
-	cd /home/kali/Downloads
+	cd $USERDIR
 	echo "$Y Beginning software installs & updates.. $NC"
 	echo ""
-	sudo apt-get install python-pip -y
+
 	sudo apt-get install sublime-text -y
 	sudo apt install seclists -y
 	sudo apt install gobuster -y
@@ -61,7 +77,7 @@ function softwareupdate() {
 	sudo neo4j start
 
 	if [[ ! -f "/usr/local/bin/one-lin3r" ]]; then
-		sudo ln -s /home/kali/.local/bin/one-lin3r /usr/local/bin/
+		sudo ln -s /home/$user/.local/bin/one-lin3r /usr/local/bin/
 	else
 		echo "$Y one-lin3r already exists in /usr/local/bin"
 		echo "$NC"
@@ -69,60 +85,60 @@ function softwareupdate() {
 	fi
 
 	# GIT CLONES
-	if [[ ! -d "/home/kali/Downloads/nmap-vulners" ]]; then
+	if [[ ! -d "/home/$user/Downloads/nmap-vulners" ]]; then
 		git clone https://github.com/vulnersCom/nmap-vulners
-	elif [[ -d "/home/kali/Downloads/nmap-vulners" ]]; then
-		cp /home/kali/Downloads/nmap-vulners/vulners.nse /usr/share/nmap/scripts/vulners.nse
+	elif [[ -d "/home/$user/Downloads/nmap-vulners" ]]; then
+		sudo cp /home/$user/Downloads/nmap-vulners/vulners.nse /usr/share/nmap/scripts/vulners.nse
 	fi
 	
-	if [[ ! -d "/home/kali/Downloads/nmapAutomator" ]]; then
+	if [[ ! -d "/home/$user/Downloads/nmapAutomator" ]]; then
 		git clone https://github.com/21y4d/nmapAutomator
-		chmod +x /home/kali/Downloads/nmapAutomator/nmapAutomator.sh
+		chmod +x /home/$user/Downloads/nmapAutomator/nmapAutomator.sh
 		echo "nmapAutomator is now executable"
 		if [[ ! -f /usr/local/bin/nmapAutomator.sh ]]; then
-			sudo ln -s /home/kali/Downloads/nmapAutomator/nmapAutomator.sh /usr/local/bin/
+			sudo ln -s /home/$user/Downloads/nmapAutomator/nmapAutomator.sh /usr/local/bin/
 		fi
 	else
 		echo "$Y Already have nmapAutomator.... continuing $NC"
 	fi
 
-	if [[ ! -d "/home/kali/Downloads/Impacket" ]]; then
+	if [[ ! -d "/home/$user/Downloads/Impacket" ]]; then
 		git clone https://github.com/SecureAuthCorp/Impacket
 	else
 		echo "$Y Already have Impacket, continuing $NC"
 	fi
-	if [[ ! -d "/home/kali/Downloads/privilege-escalation-awesome-scripts-suite" ]]; then
+	if [[ ! -d "/home/$user/Downloads/privilege-escalation-awesome-scripts-suite" ]]; then
 		git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
-		mv "/home/kali/Downloads/privilege-escalation-awesome-scripts-suite" "/home/kali/Downloads/PEAS"
+		mv "/home/$user/Downloads/privilege-escalation-awesome-scripts-suite" "/home/$user/Downloads/PEAS"
 	else
 		echo "$Y Already have LinPEAS & WinPEAS... continuing $NC"
 	fi
 
-	if [[ ! -d "/home/kali/Downloads/mitm6" ]]; then
+	if [[ ! -d "/home/$user/Downloads/mitm6" ]]; then
 		git clone https://github.com/fox-it/mitm6
-		pip3 install -r ~/home/kali/Downloads/mitm6/requirements.txt
-		python3 /home/kali/Downloads/mitm6/setup.py install
+		pip3 install -r ~/home/$user/Downloads/mitm6/requirements.txt
+		python3 /home/$user/Downloads/mitm6/setup.py install
 	else
 		echo "$Y Already have LinPEAS & WinPEAS... continuing $NC"
 	fi
 
 	if [[ ! -e "/usr/local/bin/LinEnum.sh" ]]; then
-		wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -O /home/kali/Downloads/LinEnum.sh
-		sudo ln -s /home/kali/Downloads/LinEnum.sh /usr/local/bin/
+		wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -O /home/$user/Downloads/LinEnum.sh
+		sudo ln -s /home/$user/Downloads/LinEnum.sh /usr/local/bin/
 	else
 		echo "$Y Already have LinEnum.sh... continuing $NC"
 	fi
-	if [[ ! -d "/home/kali/Downloads/evil-winrm" ]]; then
+	if [[ ! -d "/home/$user/Downloads/evil-winrm" ]]; then
 		git clone https://github.com/HackPlayers/evil-winrm
 	else
 		echo "$Y Already have evil-winrm... continuing"
 	fi
-	if [[ -d "/home/kali/Downloads/Enum4LinuxPy" ]]; then
+	if [[ -d "/home/$user/Downloads/Enum4LinuxPy" ]]; then
 		git clone https://github.com/0v3rride/Enum4LinuxPy
 	else
 		echo "$Y Already have Enum4LinuxPy... contuing"
 	fi
-	if [[ -d "/home/kali/Downloads/vlan-hopping" ]]; then
+	if [[ -d "/home/$user/Downloads/vlan-hopping" ]]; then
 		git clone https://github.com/nccgroup/vlan-hopping.git
 	else:
 		echo "$Y Already have vlan-hopping.... continuing"
@@ -153,7 +169,17 @@ function softwareupdate() {
 	fi
 }
 
-function main() {
+
+echo "$Y Automated kit buildout script"
+echo "$NC"
+
+set -eu -o pipefail # If we fail due to some error, debug all lines
+sudo -n true
+test $? -eq 0 || exit 1 "$RED ERROR: This script must be run as root"
+echo "$NC"
+
+
+function all() {
 	softwareupdate
 	systemupdate
 }
@@ -168,8 +194,7 @@ echo "$Y Or just all $NC$RED TOOLS?$NC$Y If TOOLS is selected, apt update, insta
 read input
 
 if [[ $input == "ALL" ]]; then
-	systemupdate
-	softwareupdate
+	all
 elif
 	[[ $input == "TOOLS" ]]; then
 	softwareupdate
