7066363ec3350b745d1715367470152047c6ef0e
silent6trinity <78173918+silent6trinity@users.noreply.github.com>
2023-01-10 14:05:05 -0500

    Updated dates/releases

--- tools.py
+++ tools.py
@@ -66,9 +66,9 @@ PYPI_PACKAGES = [
 
 #Holdover for now, unused
 BINARIES = [
-	'https://github.com/carlospolop/PEASS-ng/releases/download/20221009/linpeas.sh',
-	'https://github.com/carlospolop/PEASS-ng/releases/download/20221009/winPEAS.bat',
-	'https://github.com/carlospolop/PEASS-ng/releases/download/20221009/winPEASany.exe',
+	'https://github.com/carlospolop/PEASS-ng/releases/download/20230108/linpeas.sh',
+	'https://github.com/carlospolop/PEASS-ng/releases/download/20230108/winPEAS.bat',
+	'https://github.com/carlospolop/PEASS-ng/releases/download/20230108/winPEASany.exe',
 	'https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_windows_amd64.exe',
 	'https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64',
 	'https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64',
